efa5a5ef575d6c242e0531a8257b6def0d9cc5c0
Enrickyb <80066756+Enrickyb@users.noreply.github.com>
2022-03-08 17:25:52 -0400

    Adicionando 'Bem-vindo

--- script.js
+++ script.js
@@ -3,6 +3,13 @@
 //Refrigerante/Agua = 1000 ml por pessoa / se a duração for maior que 6 horas = 1500ml por pessoa
 //Crianças valem metade de uma pessoa 0.5
 
+function continuar(){
+    let welcome = document.getElementById('bv')
+    let churr = document.getElementById('bf')
+    welcome.style.display = 'none'
+
+    churr.style.display = 'flex'
+}
 
 let res = document.getElementById ('resultado')
 let adultos = document.getElementById ('adultos')
@@ -30,7 +37,7 @@ function calcular() {
     var bebidaC = crianças.value * 600
     var refri   = Math.round(bebidaC / 1000)
     var resulbebida = Math.round(bebidaA / 350)
-    res.innerHTML = (`<p>A quantidade de carne sera ${resulcarne}g</p>`)
+    res.innerHTML = (`<p>A quantidade de carne será ${resulcarne}g</p>`)
     res.innerHTML += (`<p>${resulbebida} Latas de cerveja</p>`)
     res.innerHTML += (`<p>${refri} Garrafas de 2L de refrigerante</p>`)
     } else {
